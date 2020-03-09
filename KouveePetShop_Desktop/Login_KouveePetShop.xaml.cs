3ec54dee06ed468e35e21ca643f0f6463ae90825
= <=>
2020-03-10 00:00:32 +0700

    ddwd

---
+++ KouveePetShop_Desktop/Login_KouveePetShop.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KouveePetShop_Desktop
+{
+    /// <summary>
+    /// Interaction logic for Login_KouveePetShop.xaml
+    /// </summary>
+    public partial class Login_KouveePetShop : Window
+    {
+        public Login_KouveePetShop()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtUsername_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
