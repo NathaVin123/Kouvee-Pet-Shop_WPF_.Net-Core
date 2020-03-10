f26cb1c9b42689a26427690ca0ea319383e7510b
= <=>
2020-03-10 13:43:48 +0700

    dwds

---
+++ KouveePetShop_Desktop/Layanan_KouveePetShop_CUD.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KouveePetShop_Desktop
+{
+    /// <summary>
+    /// Interaction logic for Layanan_KouveePetShop.xaml
+    /// </summary>
+    public partial class Layanan_KouveePetShop : Window
+    {
+        public Layanan_KouveePetShop()
+        {
+            InitializeComponent();
+            String connectionString = "SERVER=localhost:8081,DATABASE=backend_p3l,UID=root,PASSWORD=;";
+
+            MySqlConnection connection = new MySqlConnection
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
