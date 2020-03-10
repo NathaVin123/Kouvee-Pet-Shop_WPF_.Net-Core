6e8af2b8f886711542bb76674cfde626f028a619
Bndgld <glad_adiwijaya@yahoo.co.id>
2020-03-10 15:40:40 +0700

    perubahan keempat

--- KouveePetShop_Desktop/Layanan_KouveePetShop_CUD.xaml.cs
+++ KouveePetShop_Desktop/Layanan_KouveePetShop_CUD.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace KouveePetShop_Desktop
 {
@@ -23,25 +24,48 @@ namespace KouveePetShop_Desktop
         public Layanan_KouveePetShop()
         {
             InitializeComponent();
-            string connectionString = "SERVER=localhost;DATABASE=9127;UID=root;PASSWORD=;";
+            try {
+                string connectionString = "SERVER=localhost;DATABASE=9160;UID=root;PASSWORD=;";
 
-            MySql.Data.MySqlClient.MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
+                MySqlConnection connection = new MySqlConnection(connectionString);
 
-            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand("SELECT * FROM layanans", connection);
+                MySqlCommand cmd = new MySqlCommand("SELECT `nama_layanan`, `harga_layanan`, `jenis_layanan` FROM `layanans`", connection);
 
-            connection.Open();
+                connection.Open();
 
-            DataTable dt = new DataTable();
-            dt.Load(cmd.ExecuteReader());
-            connection.Close();
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                connection.Close();
 
-            dtLayanan.DataContext = dt;
-
-        }
+                dtLayanan.DataContext = dt;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        } 
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+           
+        }
 
+        private void ComboBox_SelectionChanged(object sender, EventArgs e)
+        {
+            InitializeComponent();
+
+            MySqlConnection connection = new MySqlConnection("Datasource=localhost;username=root;password=;");
+            connection.Open();
+            string selectQuery = "SELECT * FROM 9160.jenis_layanan";
+
+            MySqlCommand command = new MySqlCommand(selectQuery, connection);
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+               /*ComboBox.Items.Add(reader.GetString("fname"));*/
+            }
         }
     }
 }
