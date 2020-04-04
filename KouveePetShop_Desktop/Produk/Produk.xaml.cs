2d8c06ede7e57ade7f2e35c2a4f41305fd1ac6e9
Nathaniel Vincent Wibowo <170709127@students.uajy.ac.id>
2020-04-04 20:46:47 +0700

    GambarBtn blm bisa

--- KouveePetShop_Desktop/Produk/Produk.xaml.cs
+++ KouveePetShop_Desktop/Produk/Produk.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Microsoft.Win32;
 
 namespace KouveePetShop_Desktop.Produk
 {
@@ -29,6 +32,19 @@ namespace KouveePetShop_Desktop.Produk
             var Menu = new Menu.Menu_KouveePetShop();
             Menu.Show();
             this.Close();
+        }
+
+        private void GambarBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Multiselect = false;
+            opf.Filter = "Choose Image(*.JPG;*.PNG;*.GIF)|*.jpg;*.png;*.gif";
+            Nullable<bool> dialogOK = opf.ShowDialog();
+
+            if(dialogOK == true)
+            {
+                UploadImage = Image.
+            }
         }
     }
 }
