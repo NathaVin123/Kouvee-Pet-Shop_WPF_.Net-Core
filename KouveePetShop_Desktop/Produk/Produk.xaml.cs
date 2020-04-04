3b11fe35a75732be9e9d0cb6b46fe20c1ab1e348
Nathaniel Vincent Wibowo <170709127@students.uajy.ac.id>
2020-04-04 22:42:38 +0700

    B

--- KouveePetShop_Desktop/Produk/Produk.xaml.cs
+++ KouveePetShop_Desktop/Produk/Produk.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace KouveePetShop_Desktop.Produk
 {
@@ -100,6 +102,11 @@ namespace KouveePetShop_Desktop.Produk
             var Menu = new Menu.Menu_KouveePetShop();
             Menu.Show();
             this.Close();
+        }
+
+        private void GambarBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
