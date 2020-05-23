b41db1d8fe7037c0612c312ac4602311cee9494b
Nathaniel Vincent Wibowo <170709127@students.uajy.ac.id>
2020-05-23 23:15:15 +0700

    Login Bisa

--- KouveePetShop_Desktop/Login/Login_KouveePetShop.xaml.cs
+++ KouveePetShop_Desktop/Login/Login_KouveePetShop.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace KouveePetShop_Desktop
 {
@@ -19,17 +21,57 @@ namespace KouveePetShop_Desktop
     /// </summary>
     public partial class Login_KouveePetShop : Window
     {
+        MySqlConnection conn;
+        DataTable dt;
         public Login_KouveePetShop()
         {
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            try
+            {
+                conn = new MySqlConnection();
+                conn.ConnectionString = "SERVER=localhost;DATABASE=petshop;UID=root;PASSWORD=;";
+            }
+            catch
+            {
+                MessageBox.Show("Tidak ada database...");
+            }
+
         }
 
         private void BtnSubmit_Click(object sender, RoutedEventArgs e)
         {
-            var Menu = new Menu.Menu_KouveePetShop();
-            Menu.Show();
-            this.Close();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT COUNT(1) FROM pegawais WHERE NIP = @NIP AND password = @password";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@NIP", txtNIP.Text);
+                cmd.Parameters.AddWithValue("@password", txtPassword.Password);
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    var Menu = new Menu.Menu_KouveePetShop();
+                    Menu.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("NIP dan Password anda belum sesuai");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Database bermasalah...");
+            }
+            finally
+            {
+                conn.Close();
+            }
+            
         }
 
         private void KlikDisini_Click(object sender, RoutedEventArgs e)
